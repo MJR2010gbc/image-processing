862738ac3b7d149c730acf173229c59e5ab37063
Alex Vecchiettini <alex.vecchiettini@gmail.com>
2022-11-07 16:19:26 -0500

    Changed HelloWorld to display spectra

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,5 +7,22 @@ from FinalProject import ImageProcessing
 np.abs(-2)
 
 im = ImageProcessing('image1.jpeg')
+colors = ['Red', 'Green', 'Blue']
 
-im.noise_filtering()
+print(im.magnitude_spectrum[0])
+print(im.magnitude_spectrum[0].shape)
+for i, image in enumerate(im.magnitude_spectrum):
+    plt.imshow(image, cmap='gray')
+    plt.title(f'Image {colors[i]}')
+    plt.show()
+
+
+# plt.subplot(121)
+# plt.imshow(im.image)
+# plt.title('Input Image')
+# plt.xticks([]), plt.yticks([])
+# plt.subplot(122)
+# plt.imshow(im.frequency_image, cmap = 'gray')
+# plt.title('Magnitude Spectrum')
+# plt.xticks([]), plt.yticks([])
+# plt.show()
