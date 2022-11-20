ced01c48c1831bbf7249c47b734434da96708d64
Alex Vecchiettini <alex.vecchiettini@gmail.com>
2022-11-20 12:58:45 -0500

    Fixes to color images

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -4,35 +4,47 @@ from matplotlib import pyplot as plt
 
 
 class ImageProcessing:
+    
     # Contains the image path, must be passed as argument in the creation of an instance
     image_path : str
+    
     # The actual image loaded in memory and stored as numpy array of shape (height, width, colors(BGR))
     image : np.ndarray
+    
     # The actual image loaded in memory and stored as numpy array in grayscale of shape (height, width)
     image_gray : np.ndarray
-    # important data about the image
+    
+    # image dimensions
     row_number : int
     column_number : int 
+    
     # center row and col of the image (rounded)
     crow : int 
     ccol : int
+    
     # List of matrices (numpy arrays) containing the frequency domain with both natural and immaginary components 
     # of each color (sorted as RGB), already shifted to center the 0 frequencies, shape of each element of the list is (height, width, 2)
     image_frequency_RGB : list
     image_frequency_gray : np.ndarray
-    # List of spectra of the magnitude, divided by color and sorted as RGB, logaritmic scale applied 
-    magnitude_spectrum_RGB : list
+    
+    # Spectra of the magnitude, logaritmic scale applied 
+    magnitude_spectrum_RGB : list           # List divided by color and sorted as RGB
     magnitude_spectrum_gray : np.ndarray
+    
     # List of matrices (numpy arrays) containing the frequency domain with both natural and immaginary components 
     # of each color (sorted as RGB), already shifted to center the 0 frequencies, shape of each element of the list is (height, width, 2)
     # POST FILTERING
     filtered_image_frequency_RGB : list
+    filtered_image_frequency_gray : np.ndarray
+
     # List of spectra of the magnitude, divided by color and sorted as RGB, logaritmic scale applied 
     # POST FILTERING
     filtered_magnitude_spectrum_RGB : list
-    filtered_magnitude_spectrum_gray : list
+    filtered_magnitude_spectrum_gray : np.ndarray
+    
     # The actual image filtered as numpy array of shape (height, width, colors(RGB))
     filtered_image_RGB : np.ndarray
+    filtered_image_gray : np.ndarray
 
 # 
 #   Initialiazation method -> Defines the class and it's properties and instantiates all the necessary variables for easier access and 
@@ -48,7 +60,7 @@ class ImageProcessing:
         
         # Converting the image colors from BGR to RGB
         self.image = cv2.cvtColor(image_pre, cv2.COLOR_BGR2RGB)
-        self.filtered_image_RGB = cv2.cvtColor(image_pre, cv2.COLOR_BGR2RGB)
+        self.filtered_image_RGB = cv2.imread(image_path, cv2.IMREAD_COLOR)
         
         # Reading the grayscale version
         self.image_gray = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -66,11 +78,6 @@ class ImageProcessing:
         self.magnitude_spectrum_RGB = []
         self.filtered_image_frequency_RGB = []
         self.filtered_magnitude_spectrum_RGB = []
-
-        self.image_frequency_gray = []
-        self.magnitude_spectrum_gray = []
-        self.filtered_image_frequency_gray = []
-        self.filtered_magnitude_spectrum_gray = []
          
         # iterating over each color to get it's frequency domain
         for i in range(self.image.shape[2]):
@@ -109,95 +116,105 @@ class ImageProcessing:
 #    
 #   Utility methods
 #    
-    def recompose_image(self, matrix):
-        unshifted_frequency = np.fft.ifftshift(matrix)
-        image_back = cv2.idft(unshifted_frequency)
-        image_back = cv2.magnitude(image_back[:,:,0], image_back[:,:,1])
-        return image_back
-
-    # radius - radius of the mask shape, 
-    # direction - 0: all the elements inside the circular area are 0s(hpf), Note: the values are not actual 0s but a close approximation to avoid DivideByZero error
+    # Generates the (circular) mask to apply, accepts 3 arguments:
+    #  - radius -> radius of the mask shape, 
+    #  - intensity -> indicates the scaling factor to apply to the parts selected by the mask
+    #  - direction -> 0: all the elements inside the circular area are 0s(hpf), Note: the values are not actual 0s but a close approximation to avoid DivideByZero error
     #             1: all elements inside the circular area are 1s(lpf) 
-    def define_circular_mask(self, radius, intensity, direction):
+    def define_circular_mask(self, radius: float, intensity: float, direction: int):
         # Circular HPF mask, center circle is 0, remaining all ones
         mask = np.ones((self.row_number, self.column_number, 2), np.float64)
         center = [self.crow, self.ccol]
         x, y = np.ogrid[:self.row_number, :self.column_number]
         #  Mask area is a matrix of boolean, defining to which frequencies the filter will be applied to
         if direction:
-            #               This is the formula of a circle         this one means everything outside the circle
+            #               This is the formula of a circle         this one means everything OUTSIDE the circle
             mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 > radius*radius
         else:
-            #               This is the formula of a circle         this one means everything inside the circle
+            #               This is the formula of a circle         this one means everything INSIDE the circle
             mask_area = (x - center[0]) ** 2 + (y - center[1]) ** 2 <= radius*radius
         
         mask[mask_area] = intensity
         return mask
 
-    def create_color_list(self, color):
-        color_channels = []
-        if color == 1:
-            color_channels = [0, 1, 2]
-        elif color == 3:
-            color_channels.append(0)
-        elif color == 4:
-            color_channels.append(1)
-        elif color == 5:
-            color_channels.append(2)
-        return color_channels
+    # Gets back the resulting image from the frequency domain to the spatial domain
+    # accepts 1 argument:
+    #  - matrix -> the frequency domain version of a single channel of the image (2D np.array) 
+    def recompose_image(self, matrix):
+        unshifted_frequency = np.fft.ifftshift(matrix)
+        image_back = cv2.idft(unshifted_frequency, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+
+        return image_back
     
-    def get_image_back(self, color_channels, mask):
+    # Take the filtered frequency domain and generates magnitude spectrum and resulting image of the filtering
+    # takes 1 argument:
+    #  - color_channels -> list of int where:
+    #                            0 coresponds to the Red channel
+    #                            1 coresponds to the Green channel
+    #                            2 coresponds to the Blue channel
+    #                            [] empty list indicates a grayscale image  
+    def get_image_back(self, color_channels):
         if(len(color_channels)):
         # apply mask and inverse DFT on each of the selected colors
             for channel in color_channels: 
-                self.filtered_image_frequency_RGB.append(self.image_frequency_RGB[channel] * mask)
                 magnitude = cv2.magnitude(self.filtered_image_frequency_RGB[channel][:, :, 0], self.filtered_image_frequency_RGB[channel][:, :, 1])
                 self.filtered_magnitude_spectrum_RGB.append(200 * np.log(magnitude))
                 self.filtered_image_RGB[:, :, channel] = self.recompose_image(self.filtered_image_frequency_RGB[channel])
                 
-
         else:
-            self.filtered_image_frequency_gray = self.image_frequency_gray * mask
             magnitude = cv2.magnitude(self.filtered_image_frequency_gray[:, :, 0], self.filtered_image_frequency_gray[:, :, 1])
             self.filtered_magnitude_spectrum_gray = 20 * np.log(magnitude)
             self.filtered_image_gray = self.recompose_image(self.filtered_image_frequency_gray)
 
-    
-    # Edge Detection with High Pass Filter
-    def sharpening(self, color):
+
+    # Sharpening with High Pass Filter
+    def sharpening(self, color_channels):
         # Circular HPF mask, center circle is 0, remaining all ones
         mask = self.define_circular_mask(self.column_number/20, .5, 0)
-
-        color_channels = self.create_color_list(color)
+        if len(color_channels):
+            for channel in color_channels: 
+                self.filtered_image_frequency_RGB.append(self.image_frequency_RGB[channel] * mask)
+        else:
+            self.filtered_image_frequency_gray = self.image_frequency_gray * mask
         
-        self.get_image_back(color_channels, mask)
+        self.get_image_back(color_channels)
 
-    # Edge Detection with High Pass Filter
-    def blurring(self, color):
+
+    # Blurring with Low Pass Filter
+    def blurring(self, color_channels):
         # Circular HPF mask, center circle is 0, remaining all ones
         mask = self.define_circular_mask(self.ccol/15, .3, 1)
+        if len(color_channels):
+            for channel in color_channels: 
+                self.filtered_image_frequency_RGB.append(self.image_frequency_RGB[channel] * mask)
+        else:
+            self.filtered_image_frequency_gray = self.image_frequency_gray * mask
 
-        color_channels = self.create_color_list(color)
-        
-        self.get_image_back(color_channels, mask)
+        self.get_image_back(color_channels)
 
     # Edge Detection with High Pass Filter
-    def edge_detection(self, color):
+    def edge_detection(self, color_channels):
         # Circular HPF mask, center circle is 0, remaining all ones
-        mask = self.define_circular_mask(self.ccol/10, 0.000001, 0)
-
-        color_channels = self.create_color_list(color)
+        mask = self.define_circular_mask(self.ccol/20, 0.0001, 0)
+        if len(color_channels):
+            for channel in color_channels: 
+                self.filtered_image_frequency_RGB.append(self.image_frequency_RGB[channel] * mask)
+        else:
+            self.filtered_image_frequency_gray = self.image_frequency_gray * mask
         
-        self.get_image_back(color_channels, mask)
+        self.get_image_back(color_channels)
 
-    # Edge Detection with High Pass Filter
-    def noise_filtering(self, color):
+    # noise filtering with Low Pass Filter
+    def noise_filtering(self, color_channels):
         # Circular HPF mask, center circle is 0, remaining all ones
         mask = self.define_circular_mask(self.ccol/2, 0.00001, 1)
-
-        color_channels = self.create_color_list(color)
+        if len(color_channels):
+            for channel in color_channels: 
+                self.filtered_image_frequency_RGB.append(self.image_frequency_RGB[channel] * mask)
+        else:
+            self.filtered_image_frequency_gray = self.image_frequency_gray * mask
         
-        self.get_image_back(color_channels, mask)
+        self.get_image_back(color_channels)
         
         
 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
-import skimage
 import matplotlib.pyplot as plt
 from ImageProcessing import ImageProcessing 
-import cv2
 import math
-import matplotlib
 from os.path import exists
-
-
+#                                                           (pass the actual image, NOT the ImageProcessing instance)
+#                                                                             |                   (usually 'viridis' or 'gray')
+# Method to show images, accept the following parameters:                     V                           V
+# - images: list of list, a proper input format should be as follows [[1st_image_to_show: np.array, cmap_value: string, 1st_image_title_to_display: string], ... , [nth_image_to_show: np.array, cmap_value: string, nth_image_title_to_display: string]]
+# - col_n: integer, defines the numebr of column to be displayed                                                                                                        
 def show_images(images, col_n):
     row_n = math.ceil(len(images)/col_n)
     for i, curr_image in enumerate(images):
@@ -17,22 +17,32 @@ def show_images(images, col_n):
 
     plt.show()
 
+# Get info from user on which filter and color setting to apply
+# Filter choices:               Color choices:
+# 1 - blur (LPF)                1 - Color(RGB)
+# 2 - sharpening (HPF           2 - Greyscale
+# 3 - edge detection            3 - Red
+# 4 - noise filtering           4 - Green
+#                               5 - Blue
 def user_filter_choice():
     print('What transformation do you want to apply? (1 to 4)')
     print('1 - blur (LPF)')
     print('2 - sharpening (HPF)')
     print('3 - edge detection')
     print('4 - noise filtering')
-    filter_number = 0
-    while(not (filter_number in ([1, 2, 3, 4]))):
+    # Check that the user input is valid
+    while True:
         try:
             filter_number = int(input())
             if(not (filter_number in ([1, 2, 3, 4]))):
                 raise Exception("You inserted a wrong number, try again!")
-        except ValueError:
-            print('Please enter an integer.')
-        except:
-            print('You inserted a wrong number, try again!')
+            break
+        except ValueError as ve:
+            print(ve)
+            continue
+        except Exception as e:
+            print(e)
+            continue
 
     print('What version of the image you want to work with?')
     print('1 - Color(RGB)')
@@ -40,19 +50,39 @@ def user_filter_choice():
     print('3 - Red')
     print('4 - Green')
     print('5 - Blue')
-    color_number = 0
-    while(not (color_number in ([1, 2, 3, 4, 5]))):
+    # Check that the user input is valid
+    while True:
         try:
             color_number = int(input())
-            if(not (color_number in ([1, 2, 3, 4, 5]))):
+            if(not (color_number in [1, 2, 3, 4, 5])):
                 raise Exception("You inserted a wrong number, try again!")
-        except ValueError:
-            print('Please enter an integer.')
-        except:
-            print('You inserted a wrong number, try again!')
+            break
+        except ValueError as ve:
+            print(ve)
+            continue
+        except Exception as e:
+            print(e)
+            continue
     
-    return filter_number, color_number 
+    # generates a list of channels to be used for the transformations, empty list translate to grayscale
+    if color_number == 1:
+        color_channels = [0, 1, 2]
+    elif color_number == 2:
+        color_channels = []
+    elif color_number == 3:
+        color_channels = [0]
+    elif color_number == 4:
+        color_channels = [1]
+    elif color_number == 5:
+        color_channels = [2]
+    else:
+        color_channels = []
 
+    
+    return filter_number, color_channels
+ 
+
+# Calls methods from ImageProcessing class coresponding to user choices
 def apply_filter(image, filter, color):
     if(filter == 1):
         image.blurring(color)
@@ -63,35 +93,58 @@ def apply_filter(image, filter, color):
     elif(filter == 4):
         image.noise_filtering(color)
 
-    if(color == 2):
+    if color:
+        show_images([[image.image, 'viridis', 'Original RGB image'],[image.magnitude_spectrum_RGB[0], 'gray', 'Magnitude spectrum Red'],
+                    [image.filtered_image_RGB, 'viridis', 'Filtered image'],[image.filtered_magnitude_spectrum_RGB[0], 'gray', 'Filtered magnitude spectrum Red']],2)
+    else:
         show_images([[image.image_gray, 'gray', 'Original image grayscale'],[image.magnitude_spectrum_gray, 'gray', 'Magnitude spectrum grayscale'],
                     [image.filtered_image_gray, 'gray', 'Filtered image grayscale'],[image.filtered_magnitude_spectrum_gray, 'gray', 'Filtered magnitude spectrum grayscale']], 2)
-    else:
-        show_images([[image.image, 'viridis', 'Original RGB image'],[image.magnitude_spectrum_RGB[0], 'gray', 'Magnitude spectrum Red'],
-                    [image.filtered_image_RGB, 'viridis', 'Filtered image'],[image.filtered_magnitude_spectrum_RGB[0], 'gray', 'Filtered magnitude spectrum Red']], 2)
 
 def main():
-    print('Enter the image name,')
-    print('(if the image is not in the current folder indicate the relative path)')  
-    file_exists = False
-    while(not file_exists):
-        path = input()
-        file_exists = exists(path)
-        if(not file_exists):
-            print('The specified path does not exist, try again!')
+    while True:
+        print('Enter the image name,')
+        print('(if the image is not in the current folder indicate the relative path)')  
+        # Get image address and check file existence (keep iterating until a workable path is defined)
+        # Note: Only checks for file existance not the fact that the file is an actual image!!
+        while True:
+            try:
+                path = input()
+                file_exists = exists(path)
+                if(not file_exists):
+                    raise Exception('The specified path does not exist, try again!')
+                else:
+                    break
+            except Exception as e:
+                print(e)
+        # Instantiate a variable of class ImageProcessing
+        image = ImageProcessing(path)
+        # generating list of images to output (show the user what image he/she selected)
+        images_list = [[image.image, 'viridis', 'Original image'],[image.magnitude_spectrum_RGB[0], 'gray', 'Magnitude spectrum Red'],[image.magnitude_spectrum_RGB[1], 'gray', 'Magnitude spectrum Green'],[image.magnitude_spectrum_RGB[2], 'gray', 'Magnitude spectrum Blue'],
+                        [image.image_gray, 'gray', 'Greyscale image'],[image.magnitude_spectrum_gray, 'gray', 'Magnitude spectrum Greyscale']]  
+        # Call show_image method defined above to ahowcase images
+        show_images(images_list, 4)
+        # Calling the user_filter_choice defined above to get information from the user on which filter to apply and color settings
+        filter_number, color_channels = user_filter_choice()
+        # Apply the selected filter
+        apply_filter(image, filter_number, color_channels)
+        # Requires user if a new run is needed or the program should terminate
+        while True:
+            try:
+                print('Would you like to continue?(y/n)')
+                keep_going = input()
+                if keep_going.lower() == 'n':
+                    break
+                elif keep_going.lower() == 'y':
+                    break
+                else:
+                    raise Exception('Wrong input, try again!')
+            except Exception as e:
+                print(e)
+        if keep_going.lower() == 'n':
+            break
+        
 
-    image = ImageProcessing(path)
-    images_list = [[image.image, 'viridis', 'Original image'],[image.magnitude_spectrum_RGB[0], 'gray', 'Magnitude spectrum Red'],[image.magnitude_spectrum_RGB[1], 'gray', 'Magnitude spectrum Green'],[image.magnitude_spectrum_RGB[2], 'gray', 'Magnitude spectrum Blue'],
-                    [image.image_gray, 'gray', 'Greyscale image'],[image.magnitude_spectrum_gray, 'gray', 'Magnitude spectrum Greyscale']]  
-    show_images(images_list, 4)
-
-    filter_number, color_number = user_filter_choice()
-    apply_filter(image, filter_number, color_number)
-
-
-
-
-
+# check if the process running is the 'main', in that case start 
 if __name__ == '__main__':
     main()
 
